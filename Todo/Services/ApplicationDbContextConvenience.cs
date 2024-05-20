4fac136eba26d7252ed37794e2c6c9c515283841
Anth Richardson <anthrblahblah@gmail.com>
2024-05-20 13:36:10 +0100

    ApplicationDbContextConvenience.RelevantTodoLists
    Return TodoLsts where the given user is responsible for an Item

--- Todo/Services/ApplicationDbContextConvenience.cs
+++ Todo/Services/ApplicationDbContextConvenience.cs
@@ -9,14 +9,16 @@ namespace Todo.Services
     {
         public static IQueryable<TodoList> RelevantTodoLists(this ApplicationDbContext dbContext, string userId)
         {
-            return dbContext.TodoLists.Include(tl => tl.Owner)
+            return dbContext.TodoLists
+                .Include(tl => tl.Owner)
                 .Include(tl => tl.Items)
-                .Where(tl => tl.Owner.Id == userId);
+                .Where(tl => tl.Owner.Id == userId || tl.Items.Any(i => i.ResponsibleParty.Id == userId));
         }
 
         public static TodoList SingleTodoList(this ApplicationDbContext dbContext, int todoListId)
         {
-            return dbContext.TodoLists.Include(tl => tl.Owner)
+            return dbContext.TodoLists
+                .Include(tl => tl.Owner)
                 .Include(tl => tl.Items)
                 .ThenInclude(ti => ti.ResponsibleParty)
                 .Single(tl => tl.TodoListId == todoListId);

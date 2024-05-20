553c897853ca5b3ae812891718dfa4208738c453
Anth Richardson <anthrblahblah@gmail.com>
2024-05-20 14:05:44 +0100

    TodoItemEditFieldsFactory: include rank when editing

---
+++ Todo.Tests/WhenEditingATodoItem.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Identity;
+using Todo.Data.Entities;
+using Todo.EntityModelMappers.TodoItems;
+using Xunit;
+
+namespace Todo.Tests
+{
+    public class WhenEditingATodoItem
+    {
+        [Fact]
+        public void The_rank_is_updated()
+        {
+            // Arrange
+            var todoList = new TestTodoListBuilder(new IdentityUser("alice@example.com"), "shopping")
+                .WithItem("bread", Importance.High)
+                .Build();
+            var item = todoList.Items.First();
+            var editFields = TodoItemEditFieldsFactory.Create(item);
+            editFields.Rank = 11;
+
+            // Act
+            TodoItemEditFieldsFactory.Update(editFields, item);
+
+            // Assert
+            Assert.Equal(11, item.Rank);
+        }
+    }
+}
--- Todo/EntityModelMappers/TodoItems/TodoItemEditFieldsFactory.cs
+++ Todo/EntityModelMappers/TodoItems/TodoItemEditFieldsFactory.cs
@@ -25,6 +25,7 @@ namespace Todo.EntityModelMappers.TodoItems
             dest.IsDone = src.IsDone;
             dest.ResponsiblePartyId = src.ResponsiblePartyId;
             dest.Importance = src.Importance;
+            dest.Rank = src.Rank;
         }
     }
 }

f66661b047e356d90931dcc532dd9b50a7639b07
Anth Richardson <anthrblahblah@gmail.com>
2024-05-20 12:31:57 +0100

    TodoList Detail view: hide completed checkbox

--- Todo/Models/TodoLists/TodoListDetailViewmodel.cs
+++ Todo/Models/TodoLists/TodoListDetailViewmodel.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel;
 using Todo.Models.TodoItems;
 
 namespace Todo.Models.TodoLists
@@ -8,12 +9,16 @@ namespace Todo.Models.TodoLists
         public int TodoListId { get; }
         public string Title { get; }
         public ICollection<TodoItemSummaryViewmodel> Items { get; }
+        [DisplayName("Hide Completed?")]
+        public bool HideCompleted { get; }
 
-        public TodoListDetailViewmodel(int todoListId, string title, ICollection<TodoItemSummaryViewmodel> items)
+        public TodoListDetailViewmodel(
+            int todoListId, string title, ICollection<TodoItemSummaryViewmodel> items, bool hideCompleted = false)
         {
             Items = items;
             TodoListId = todoListId;
             Title = title;
+            HideCompleted = hideCompleted;
         }
     }
 }
